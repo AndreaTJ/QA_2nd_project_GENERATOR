3a9469d445ef7c702c94f377dcd0f78956525987
AndreaTJ <63742291+AndreaTJ@users.noreply.github.com>
2021-03-01 09:04:23 +0000

    Update test_app_1.py

--- service1/testing/test_app_1.py
+++ service1/testing/test_app_1.py
@@ -4,37 +4,6 @@ from flask_testing import TestCase
 
 from app import app, db, Duo
 
-class TestBase (TestCase):
-    def create_app (self): 
-        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
-        TESTING = True)
-
-        return app
-        
-    def setUp (self): 
-        db.create_all()
-        db.session.add (Duo (country= "Mexico", money = "5000"))
-        db.session.commit()
-
-        
- 
-    def tearDown (self):
-        db.session.remove()
-        db.drop_all()
-
-
-class Test_Response(TestBase):
-    def test_request(self):
-        with patch("requests.get") as g:
-            g.return_value.text = "{'Rome': '125000.0'}"
-
-            response = self.client.get(url_for("hello_internet"))
-            self.assertIn(b"Rome", response.data)
-
-    def test_saving_records(self):
-        db.create_all()
-        db.session.add (Duo (country= "Paris", money = "5000"))
-        db.session.commit()
 
         
         
