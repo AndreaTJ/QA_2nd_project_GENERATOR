691b192c2cbfa20c0a788fdcad951380df070b56
AndreaTJ <63742291+AndreaTJ@users.noreply.github.com>
2021-03-01 00:01:07 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,17 +3,17 @@ pwd
 
 cd ./service1/testing
 pip3 install -r requirements.txt
-python3 -m pytest --cov app 
+pytest --cov app 
 cd ..
 
 cd ./service2
-python3 -m pytest --cov app
+pytest --cov app
 cd ..
 
 cd ./service3
-python3 -m pytest --cov app
+pytest --cov app
 cd ..
 
 cd ./service4
-python3 -m pytest --cov app
+pytest --cov app
 cd ..
