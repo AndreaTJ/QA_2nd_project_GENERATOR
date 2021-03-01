bce40d1ee781d58c7d170c40e4ef8d29885cb4c3
AndreaTJ <63742291+AndreaTJ@users.noreply.github.com>
2021-03-01 18:21:52 +0000

    Update test_app_1.py

--- service1/testing/test_app_1.py
+++ service1/testing/test_app_1.py
@@ -1,15 +1,40 @@
 from unittest.mock import patch
-from flask import url_for, request
+from flask import url_for, request, jsonify
 from flask_testing import TestCase
 
-from app import app
+
+from app import app, Duo, db
 
 class TestBase (TestCase):
     def create_app (self): 
+        
         app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
         TESTING = True)
 
         return app
+    
+    def setUp (self): 
+        db.create_all()
+ 
+    def tearDown (self):
+        db.drop_all()
+    
+
+class TestResponse(TestBase):
+
+    def test_req(self):
+        with patch('requests.get') as g:
+            country = "Madrid"
+            money2 = "500"
+            result_2 = country, money2
+            g.return_value.json.return_value = result_2
+
+            response = self.client.get(url_for('hello_internet'))
+            self.assertIn(b"Madrid", response.data)
+            self.assertIn(b"50", response.data)
+
+
+
 
 
 
